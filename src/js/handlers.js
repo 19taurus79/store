21a315dc4823a6a896143b5242e13ea7dafd72b7
19taurus79 <19taurus79@gmail.com>
2025-04-12 18:58:55 +0300

    initial commit

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,1 +1,51 @@
 // Функції, які передаються колбеками в addEventListners
+import { getCategories, getProducts, getProductsByCategory } from './products-api';
+import { renderCategories, renderProducts } from './render-function';
+
+// Функція викликається при завантаженні сторінки
+export async function onDOMContentLoaded() {
+  try {
+    const categories = await getCategories();
+    renderCategories(categories);
+
+    const { products } = await getProducts();
+    renderProducts(products);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// Функція викликається при кліку на категорію
+export async function onCategoryClick(event) {
+  try {
+    if (event.target.tagName !== 'BUTTON') {
+      return;   
+    }
+    const category = event.target.textContent;
+    const categoryBtn = event.target
+    const notFound = document.querySelector('.not-found');
+    console.log(category)
+    if (category === 'All') {
+      const { products } = await getProducts();
+      console.log('all',products);
+      
+      renderProducts(products);
+      return;
+    }
+    const { products } = await getProductsByCategory(category);
+    console.log(products.length);
+    
+    if (products.length === 0) {
+      console.log('notFound');
+      notFound.classList.add('not-found--visible');
+      return;
+     };
+    categoryBtn.classList.add('categories__btn--active');
+    //TODO: remove
+    console.log(products);
+    console.log(categoryBtn);
+    
+    renderProducts(products);
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,38 @@
 // Функції для роботи з бекендом
+
+import axios from 'axios';
+// Добавляємо базовий URL для всіх запитів
+axios.defaults.baseURL = 'https://dummyjson.com/products';
+// Функція для отримання категорій
+export async function getCategories() {
+  const { data } = await axios.get('/category-list');
+  return data;
+}
+// Функція для отримання продуктів за категорією
+// Використовується пагінація для отримання 12 продуктів за раз
+export async function getProductsByCategory(category, page = 1) {
+  const { data } = await axios.get(
+    `/category/${category}?limit=12&skip=${(page - 1) * 12}`
+  );
+  return data;
+}
+// Функція для отримання всіх продуктів
+// Використовується пагінація для отримання 12 продуктів за раз
+export async function getProducts(page = 1) {
+  const { data } = await axios.get(`?limit=12&skip=${(page - 1) * 12}`);
+  return data;
+}
+// Функція для отримання продукту за ID
+// Використовується пагінація для отримання 12 продуктів за раз
+export async function getProductById(id) {
+  const { data } = await axios.get(`/${id}`);
+  return data;
+}
+// Функція для отримання продуктів за запитом
+// Використовується пагінація для отримання 12 продуктів за раз
+export async function getProductsByQuery(query, page = 1) {
+  const { data } = await axios.get(
+    `/search?q=${query}&limit=12&skip=${(page - 1) * 12}`
+  );
+  return data;
+}
