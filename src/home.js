f3966d1b62c47394c76cad65589c0e8f9e9744fc
19taurus79 <19taurus79@gmail.com>
2025-04-12 19:02:01 +0300

    edit home.js

--- src/home.js
+++ src/home.js
@@ -1,1 +1,6 @@
+import { onCategoryClick, onDOMContentLoaded } from './js/handlers';
+
 //Логіка сторінки Home
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+
+document.addEventListener('click', (onCategoryClick));
